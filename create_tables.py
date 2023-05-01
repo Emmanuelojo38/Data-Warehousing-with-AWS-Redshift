205f76bd93a753fd5ee0378cdc2f3e8a5e43e6eb
Emmanuelojo38 <lanremj@gmail.com>
2023-05-01 03:40:44 +0100

    my final commit

---
+++ create_tables.py
@@ -0,0 +1,71 @@
+import configparser
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
+
+
+def drop_tables(cur, conn):
+    """
+    Description: Drops each table using the queries in 'drop_table_queries' list in sql_queries.
+    
+    Arguments:
+        cur: the cursor object.
+        conn: connection object to redshift.
+        
+    Returns:
+        None
+    """
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def create_tables(cur, conn):
+    """
+    Description: Creates each table using the queries in 'create_table_queries' list in sql_queries.
+    
+    Arguments:
+        cur: the cursor object.
+        conn: connection object to redshift.
+        
+    Returns:
+        None
+    """
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    """
+    Description:
+        - Establishes connection with the sparkify database and gets
+        cursor to it (on AWS redshift cluster created earlier).
+        
+        - Drops all the tables.
+        
+        - Creates all tables needed.
+        
+        - Finally, closes the connection.
+        
+    Returns:
+        None
+    """
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect(host = config.get("CLUSTER", "HOST"),
+                             dbname = config.get("CLUSTER", "DB_NAME"),
+                             user = config.get("CLUSTER", "DB_USER"),
+                             password = config.get("CLUSTER", "DB_PASSWORD"),
+                             port = config.get("CLUSTER", "DB_PORT")
+                            )
+    cur = conn.cursor()
+
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
